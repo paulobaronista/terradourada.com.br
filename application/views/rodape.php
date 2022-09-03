bd53dfd7e312aabdf2b8cc9b8c629c9f51ea3a6b
Igor Sorrilha <igor.sorrilha@gmail.com>
2022-09-03 03:09:49 -0300

    modal do filme inserido

--- application/views/rodape.php
+++ application/views/rodape.php
@@ -8,3 +8,17 @@
         </div>
     </div>
 </footer>
+
+<!-- Modal Video -->
+<div class="modal fade modalVideo" id="modalVideo" tabindex="-1" role="dialog" aria-labelledby="modalVideo" aria-hidden="true">
+    <div class="modal-dialog  modal-lg" role="document">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">X</button>
+            <div class="modal-body">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/uxt4ivnKLgk"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
